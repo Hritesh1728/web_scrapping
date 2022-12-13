623d93fd38615196c3e4f80bb88df1a6d115c1e3
Hritesh1728 <hriteshalw1728.com>
2022-12-13 16:34:45 +0530

    ajmer added

--- test_playwright.py
+++ test_playwright.py
@@ -1,7 +1,7 @@
 import pathlib
-import time
-from playwright.sync_api import Playwright, sync_playwright, expect
-import csv
+
+from playwright.sync_api import Playwright, sync_playwright
+
 import pandas as pd
 
 
@@ -10,14 +10,15 @@ def automate_jodhpur():
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
-        page.goto("https://jansoochna.rajasthan.gov.in/Services/")
+        page.goto("https://jansoochna.rajasthan.gov.in/Services/", wait_until="load", timeout=60000)
 
         page.get_by_role("link", name="68.) Know about your Electricity Bill Payment Information - JDVVNL").click()
         page.get_by_placeholder("Enter your K number").click()
+        page.get_by_text("Enter your K number").wait_for(timeout=60000)
 
-        # csv impoting file
+        # csv importing file
         print('welcome to JDVVNL Bill Status')
-        df = pd.read_csv(pathlib.Path().absolute().joinpath('data/source.csv'))
+        df = pd.read_csv(pathlib.Path().absolute().joinpath('data/jodhpur/source.csv'))
         df.drop_duplicates()
         df.dropna()
 
@@ -30,9 +31,8 @@ def automate_jodhpur():
                 break
             print(f'Working for {row["kno"]} with ID: {index}')
             page.get_by_placeholder("Enter your K number").fill(f'{row["kno"]}')
-            time.sleep(5)
-            page.get_by_role("button", name="खोजें").click()
 
+            page.get_by_role("button", name="खोजें").click()
             data["k_number"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(1)").inner_text()
             data["name"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(2)").inner_text()
             data["address"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(3)").inner_text()
@@ -55,7 +55,7 @@ def automate_jodhpur():
             output.append(list(data.values()))
 
         result = pd.DataFrame(output, columns=list(data.keys()))
-        result.to_csv(pathlib.Path().absolute().joinpath('data/result.csv'), index=False)
+        result.to_csv(pathlib.Path().absolute().joinpath('data/jodhpur/result.csv'), index=False)
 
         print('Bye')
 
@@ -67,64 +67,69 @@ def automate_jodhpur():
         run(playwright)
 
 
-automate_jodhpur()
-# def automate_ajmer():
-#     def run(playwright: Playwright) -> None:
-#         browser = playwright.chromium.launch(headless=False)
-#         context = browser.new_context()
-#         page = context.new_page()
-#         page.goto("https://jansoochna.rajasthan.gov.in/Services/")
-#
-#         page.get_by_role("link", name="69.) Know about your Electricity Bill Payment Information - AVVNL").click()
-#         page.get_by_placeholder("Enter your K number").click()
-#
-#         # csv impoting file
-#         print('welcome to AVVNL Bill Status')
-#         df = pd.read_csv(pathlib.Path().absolute().joinpath('data/source.csv'))
-#         df.drop_duplicates()
-#         df.dropna()
-#
-#         # output = pd.DataFrame({})
-#
-#         data = {}
-#         output = []
-#         for index, row in df.iterrows():
-#             if index == -1:
-#                 break
-#             print(f'Working for {row["kno"]} with ID: {index}')
-#             page.get_by_placeholder("Enter your K number").fill(f'{row["kno"]}')
-#             time.sleep(5)
-#             page.get_by_role("button", name="खोजें").click()
-#
-#             data["k_number"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(1)").inner_text()
-#             data["name"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(2)").inner_text()
-#             data["address"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(3)").inner_text()
-#             data["account_number"] = page.locator(
-#                 "#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(4)").inner_text()
-#             data["bill_number"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(5)").inner_text()
-#             data["receipt_number"] = page.locator(
-#                 "#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(6)").inner_text()
-#             data["billing_month"] = page.locator(
-#                 "#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(7)").inner_text()
-#             data["payment_mode"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(8)").inner_text()
-#             data["reference_number"] = page.locator(
-#                 "#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(9)").inner_text()
-#             data["issue_date"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(10)").inner_text()
-#             data["issue_bank"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(11)").inner_text()
-#             data["bill_amount_(rs)"] = page.locator(
-#                 "#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(12)").inner_text()
-#             data["receipt_date"] = page.locator(
-#                 "#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(13)").inner_text()
-#             output.append(list(data.values()))
-#
-#         result = pd.DataFrame(output, columns=list(data.keys()))
-#         result.to_csv(pathlib.Path().absolute().joinpath('data/result.csv'), index=False)
-#
-#         print('Bye')
-#
-#         # ---------------------
-#         context.close()
-#         browser.close()
-#
-#     with sync_playwright() as playwright:
-#         run(playwright)
+def automate_ajmer():
+    def run(playwright: Playwright) -> None:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://jansoochna.rajasthan.gov.in/Services/", wait_until="load", timeout=60000)
+
+        page.get_by_role("link", name="69.) Know about your Electricity Bill Payment Information - AVVNL").click()
+        page.get_by_placeholder("Enter your K number").click()
+        page.get_by_text("Enter your K number").wait_for(timeout=60000)
+
+        # csv importing file
+        print('welcome to AVVNL Bill Status')
+        df = pd.read_csv(pathlib.Path().absolute().joinpath('data/ajmer/source.csv'))
+        df.drop_duplicates()
+        df.dropna()
+
+        # output = pd.DataFrame({})
+
+        data1 = {}
+        output = []
+        for index, row in df.iterrows():
+            if index == -1:
+                break
+            print(f'Working for {row["kno"]} with ID: {index}')
+            page.get_by_placeholder("Enter your K number").fill(f'{row["kno"]}')
+
+            page.get_by_role("button", name="खोजें").click()
+            page.wait_for_timeout(3000)
+
+            data1["k_number"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(1)").inner_text()
+            data1["name"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(2)").inner_text()
+            data1["address"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(3)").inner_text()
+            data1["account_number"] = page.locator(
+                "#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(4)").inner_text()
+            data1["bill_number"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(5)").inner_text()
+            data1["receipt_number"] = page.locator(
+                "#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(6)").inner_text()
+            data1["billing_month"] = page.locator(
+                "#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(7)").inner_text()
+            data1["payment_mode"] = page.locator(
+                "#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(8)").inner_text()
+            data1["reference_number"] = page.locator(
+                "#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(9)").inner_text()
+            data1["issue_date"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(10)").inner_text()
+            data1["issue_bank"] = page.locator("#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(11)").inner_text()
+            data1["bill_amount_(rs)"] = page.locator(
+                "#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(12)").inner_text()
+            data1["receipt_date"] = page.locator(
+                "#tblResult_0 > tbody > tr:nth-child(1) > td:nth-child(13)").inner_text()
+            output.append(list(data1.values()))
+
+        result = pd.DataFrame(output, columns=list(data1.keys()))
+        result.to_csv(pathlib.Path().absolute().joinpath('data/ajmer/result.csv'), index=False)
+
+        print('Bye')
+
+        # ---------------------
+        context.close()
+        browser.close()
+
+    with sync_playwright() as playwright:
+        run(playwright)
+
+
+
